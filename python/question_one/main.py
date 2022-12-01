e25e0ee940b7cecc82bf1b4eba9f8ee0daac859f
Dennis Reitsma <dennis.reitsma@ndwcloud.nu>
2022-12-01 13:41:53 +0100

    answer(AOC): Python solution for AOC q1

---
+++ python/question_one/main.py
@@ -0,0 +1,10 @@
+from python.question_one.question_one import get_highest_value
+
+
+def main() -> None:
+    value = get_highest_value()
+    print(value)
+
+
+if __name__ == '__main__':
+    main()
