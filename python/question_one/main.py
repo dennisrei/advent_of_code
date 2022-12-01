5f8103e090dfa43983eb3a7cfcaeb3c8dc6a74dc
Dennis Reitsma <dennis.reitsma@ndwcloud.nu>
2022-12-01 15:15:45 +0100

    feat(AOC): complete answers to aoc question 1

--- python/question_one/main.py
+++ python/question_one/main.py
@@ -1,9 +1,11 @@
-from python.question_one.question_one import get_highest_value
+from python.question_one import question_one
 
 
 def main() -> None:
-    value = get_highest_value()
-    print(value)
+    list_of_ints = question_one.get_highest_value()
+    print(max(list_of_ints))
+    three_highest_values = question_one.get_three_highest_values(list_of_ints)
+    print(sum(three_highest_values))
 
 
 if __name__ == '__main__':
